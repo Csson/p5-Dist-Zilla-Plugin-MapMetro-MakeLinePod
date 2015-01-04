bca3c41f5810e27d0d9ceffb08495a18dd5348fe
Erik Carlsson <info@code301.com>
2015-01-05 00:56:35 +0100

    Init

---
+++ lib/Dist/Zilla/Plugin/MapMetro/MakeLinePod.pm
@@ -0,0 +1,45 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::MapMetro::MakeLinePod;
+
+# VERSION
+
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Zilla::Plugin::MapMetro::MakeLinePod - Short intro
+
+=head1 SYNOPSIS
+
+  use Dist::Zilla::Plugin::MapMetro::MakeLinePod;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::MapMetro::MakeLinePod is ...
+
+=head1 SEE ALSO
+
+
+=head1 AUTHOR
+
+Erik Carlsson E<lt>info@code301.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015 - Erik Carlsson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
